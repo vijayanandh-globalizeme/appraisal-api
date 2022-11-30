ab9fe68559b73b4e335fe07ccff746fd905728e5
Vijay Anandh <vijay.anandh@globalizeme.com>
2022-11-30 12:47:45 +0530

    fix

--- app/Http/Controllers/API/V1/AuthController.php
+++ app/Http/Controllers/API/V1/AuthController.php
@@ -79,6 +79,7 @@ class AuthController extends Controller
                                 )->toDateTimeString()
                             ]);
                             $appURL = config('azure.ngAppUri');
+                            dd($data);
                             return redirect()->away($appURL.'/#/login?'.$data);
                         }
                     } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
