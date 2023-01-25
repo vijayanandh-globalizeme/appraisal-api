36f10d5bdc4121884fa5a827d98b87a142290027
Vijay Anandh <vijay.anandh@globalizeme.com>
2023-01-25 11:01:59 +0530

    Update migration file

--- database/migrations/2022_10_09_173521_create_review_questions.php
+++ database/migrations/2022_10_09_173521_create_review_questions.php
@@ -15,8 +15,12 @@ class CreateReviewQuestions extends Migration
     {
         Schema::create('review_questions', function (Blueprint $table) {
             $table->id();
+            $table->integer('category_id');
             $table->string('name', 50);
             $table->string('label');
+            $table->enum('status', ['TEXT','TEXTAREA','RADIO','CHECKBOX','SELECT','STAR']);
+            $table->tinyInteger('isRequired')->default('1');
+            $table->longText('options');
             $table->tinyInteger('status')->default('1');
             $table->timestamps();
         });
