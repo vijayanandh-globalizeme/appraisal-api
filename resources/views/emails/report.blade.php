0de92e5f52826f0861b6eae62e7efeac802f5b57
Vijay Anandh <vijay.anandh@globalizeme.com>
2023-02-22 23:54:20 +0530

    Fix type

--- resources/views/emails/report.blade.php
+++ resources/views/emails/report.blade.php
@@ -4,7 +4,7 @@
     <title>GM360</title>
 </head>
 <body>
-    <p>Dear HR team,</p>
+    <p>Dear HR Team,</p>
     <p>I am delighted to present to you the recently submitted Annual Review Report for your team member.</p>
     <p>Please refer to the attached document for a comprehensive overview.</p><br/>
     <p>Thank you</p>
